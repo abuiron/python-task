db501445c2eb52a4006ad47bef30d70abce940d6
abuiron <abuiron80@gmail.com>
2024-03-14 07:20:48 +0530

    RPS modified

--- Rock,Paper,Scissors game.py
+++ Rock,Paper,Scissors game.py
@@ -3,12 +3,12 @@ while True:
     print('\n\t .....Rock,Paper,Scissor game.....')
     choice=input('\nEnter your choice :')
 
-    game_choices=['stone','paper','scissor']
+    game_choices=['rock','paper','scissor']
 
     opposite=random.choice(game_choices)
     print('\n ENEMY :',opposite)
 
-    if(choice=='stone' and opposite=='scissor' or choice=='scissor' and opposite=='paper' or choice=='paper' and opposite=='stone'):
+    if(choice=='rock' and opposite=='scissor' or choice=='scissor' and opposite=='paper' or choice=='paper' and opposite=='rock'):
         print('\n Your win :)')
     elif(choice==opposite):
         print('\n Tie')
